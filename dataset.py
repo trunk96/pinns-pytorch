added derivative as output of the network, refactored the datasets to be correct and more efficient

dataset.py
@@ -13,11 +13,15 @@ class DomainDataset(Dataset):
         self.n = n
         self.side_length = self.xmax- self.xmin
         self.volume = np.prod(self.side_length)
+        self.compute_items()
 
     def __len__(self):
-        return self.n
+        return self.x.shape[0]
     
     def __getitem__(self, idx):
+        return self.x[idx]
+        
+    def compute_items(self):    
         dx = (self.volume / self.n) ** (1 / self.dim)
         xi = []
         for i in range(self.dim):
@@ -30,7 +34,7 @@ class DomainDataset(Dataset):
             print(
                 "Warning: {} points required, but {} points sampled.".format(self.n, len(x))
             ) """
-        return x[idx]
+        self.x = x
     
 
 class ICDataset(DomainDataset):
@@ -38,6 +42,9 @@ class ICDataset(DomainDataset):
         super().__init__(xmin, xmax, n)
     
     def __getitem__(self, idx):
+        return self.x[idx]
+    
+    def compute_items(self):
         dx = (self.volume / self.n) ** (1 / self.dim)
         xi = []
         for i in range(self.dim):
@@ -53,7 +60,7 @@ class ICDataset(DomainDataset):
             #print(
             #    "Warning: {} points required, but {} points sampled.".format(self.n, len(x))
             #)
-        return x[idx]
+        self.x = x
 
    
 class BCDataset(DomainDataset):

main.py
@@ -19,12 +19,16 @@ def pde_fn(prediction, sample):
     # dudx = torch.autograd.grad(prediction, sample, create_graph=True, retain_graph=True)[0][0][0]
     # dduddt = torch.autograd.grad(dudt, sample, retain_graph=True)[0][0][-1].reshape((1, 1))
     # dduddx = torch.autograd.grad(dudx, sample, retain_graph=True)[0][0][0].reshape((1, 1))
-    d = torch.autograd.grad(prediction, sample, grad_outputs=torch.ones_like(prediction),create_graph = True,only_inputs=True)[0]
-    dd = torch.autograd.grad(d, sample, grad_outputs=torch.ones_like(d),create_graph = True,only_inputs=True)[0]
+    grads = torch.zeros_like(prediction)
+    grads[:, 0] = 1
+    d = torch.autograd.grad(prediction, sample, grad_outputs=grads,create_graph = True, materialize_grads = True)[0]
+    dd = torch.autograd.grad(d, sample, grad_outputs=torch.ones_like(d),create_graph = True)[0]
     a = 1
     return dd[:, -1] - a*dd[:, 0]
 
 def interpolate(x, points, ic_points):
+    if x in points:
+        return ic_points[points.index(x)]
     selected_points = []
     for i in range(len(points)-1):
         if points[i] <= x and points[i+1] >= x:
@@ -33,29 +37,35 @@ def interpolate(x, points, ic_points):
     return u_ic
 
 def ic_fn_pos(prediction, sample):
-    x = sample[0][0]
+    x = sample[:, 0]
     points = [0, 0.25, 0.5, 0.75, 1]
-    ic_points = [0, sample[0][1], sample[0][2], sample[0][3], 0]
-    ic = interpolate(x, points, ic_points)
-    ic = torch.Tensor(ic).to(device=prediction.device)#.reshape(prediction.shape)
-    return prediction, ic
+    ics = []
+    for i in range(x.shape[0]):
+        ic_points = [0, sample[i, 1], sample[i, 2], sample[i, 3], 0]
+        ic = interpolate(x[i], points, ic_points)
+        ics.append(ic)
+    ics = torch.Tensor(ics).to(device=prediction.device)#.reshape(prediction.shape)
+    return prediction[:, 0], ics
 
 def ic_fn_vel(prediction, sample):
-    x = sample[0][0]
+    x = sample[:, 0]
     points = [0, 0.25, 0.5, 0.75, 1]
-    ic_points = [0, sample[0][4], sample[0][5], sample[0][6], 0]
-    dudt = torch.autograd.grad(prediction, sample, grad_outputs=torch.ones_like(prediction),create_graph = True,only_inputs=True)[0][:, -1]
-    # dudt = dudt.reshape((1,1))
-    ic = interpolate(x, points, ic_points)
-    ic = torch.Tensor(ic).to(device=dudt.device)#.reshape(dudt.shape)
-    return dudt, ic
+    ics = []
+    for i in range(x.shape[0]):
+        ic_points = [0, sample[0][4], sample[0][5], sample[0][6], 0]
+        ic = interpolate(x[i], points, ic_points)
+        ics.append(ic)
+    #dudt = torch.autograd.grad(prediction, sample, grad_outputs=torch.ones_like(prediction),create_graph = True,only_inputs=True)[0][:, -1]
+    # dudt = dudt.reshape((1,1))  
+    ics = torch.Tensor(ics).to(device=prediction.device)#.reshape(dudt.shape)
+    return prediction[:, -1], ics
 
 
 
-batchsize = 32
+batchsize = 128
 learning_rate = 1e-3 
 
-domainDataset = DomainDataset([0.0]*8, [1.0]*8, 1000)
+domainDataset = DomainDataset([0.0]*8, [1.0]*7 + [0.05], 1000)
 icDataset = ICDataset([0.0]*8, [1.0]*8, 1000)
 
 model = PINN([8] + [100]*3 + [1], nn.Tanh, hard_constraint).to(torch.device('cuda:0'))

train.py
@@ -50,7 +50,9 @@ def train(name, model, epochs, batchsize, optimizer, pde_fn, ic_fns, domaindatas
                         100. * batch_idx / len(dataloader), loss.item()))
                     
                     losses.append(loss.item())  # Storing the loss
-
+            if epoch % 20 == 0:
+                epoch_path = os.path.join(model_dir, f"{name}_{epoch}.pt")
+                torch.save(model, epoch_path)
     # Save the model
     torch.save(model, model_path)
     