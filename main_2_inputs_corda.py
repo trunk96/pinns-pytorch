2052f7c9d432360398be42b1d6e2fa4c2ad797b9
Emanuele De Santis <trunk96@gmail.com>
2024-04-17 16:56:00 +0200

    fixed velocity IC

--- main_2_inputs_corda.py
+++ main_2_inputs_corda.py
@@ -46,7 +46,7 @@ def ic_fn_pos(prediction, sample):
     return prediction[:, 0], ics
 
 def ic_fn_vel(prediction, sample):
-    ics = torch.zeros_like(prediction[:, -1])
+    ics = torch.zeros_like(prediction[:, 0])
     dt = jacobian(prediction, sample, i=1, j=1)
     return dt, ics
 
