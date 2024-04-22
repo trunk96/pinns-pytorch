6e02d94ecbbc79b212abd3ec2635038a81971659
Emanuele De Santis <trunk96@gmail.com>
2024-04-22 15:34:28 +0200

    added possibility to plot experiments with ic on velocity different from 0. Added new experiment

--- plot_corda_semplice.py
+++ plot_corda_semplice.py
@@ -10,7 +10,7 @@ from pinns.train import train
 from pinns.dataset import DomainDataset, ICDataset
 
 name = "output"
-experiment_name = "string_2inputs_nostiffness_icsin_icv0_1"
+experiment_name = "string_2inputs_nostiffness_ic0hard_icvsin_1"
 current_file = os.path.abspath(__file__)
 output_dir = os.path.join(os.path.dirname(current_file), name)
 output_dir = os.path.join(output_dir, experiment_name)
@@ -25,27 +25,40 @@ a = 1
 
 
 def w1(x):
-
-    return np.sin(x*np.pi)
+    return np.zeros_like(x)
+    #return np.sin(x*np.pi)
 
 
 def w2(z):
 
-    return 0
+    return np.sin(z*np.pi)
 
 
+# TO BE USED IF w1 = 0 and w2 != 0
 def exact(x):
-    x, t = np.split(x, 2, axis=1)
+    a = 1
+    ex = []
+    for elem in x:
+        y, _, = quad(w2, (elem[0]-a*elem[1]), (elem[0]+a*elem[1]))
+        ex.append(1/(2*a) * y)
+    return np.array(ex)
 
-    return (w1(x-a*t) + w1(x+a*t))/2
+""" 
+# TO BE USED IF w1 != 0 and w2 = 0
+def exact(x):
+    a = 1
+    x, t = np.split(x, 2, axis=1)
+    return (w1(x-a*t) + w1(x+a*t))/2 
+"""
+
 
 
 def hard_constraint(x, y):
-    res = x[:, 0:1] * (1 - x[:, 0:1]) * y
+    res = x[:, 0:1].reshape(-1, 1) * (1 - x[:, 0:1].reshape(-1, 1)) * y * x[:, 1].reshape(-1, 1)
     return res
 
 
-batchsize = 1000
+batchsize = 10000
 learning_rate = 1e-3 
 
 domainDataset = DomainDataset([0.0]*2, [1.0]*2, 1000)
---
+++ string_2_inputs_force.py
@@ -0,0 +1,91 @@
+from pinns.model import PINN
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from pinns.train import train
+from pinns.gradient import jacobian
+from pinns.dataset import DomainDataset, ICDataset, ValidationDataset, ValidationICDataset
+
+
+epochs = 1000
+num_inputs = 2 #x, t
+
+
+def hard_constraint(x, y):
+    return x[:, 0].reshape(-1, 1) * (1 - x[:, 0]).reshape(-1, 1) * y * x[:, -1].reshape(-1, 1)
+
+def f(sample):
+    x = sample[:, 0].reshape(-1, 1)
+    #y = sample[:, 1]
+    """ x_f = sample[:, 2]
+    y_f = sample[:, 3]
+    height = sample[:, 4] """
+    x_f = 0.8
+    #y_f = 0.8
+    height = 1
+    t = sample[:, -1].rehsape(-1, 1)
+
+    alpha = 8.9
+    za = -height * torch.exp(-400*((x-x_f)**2)) * (4**alpha * t**(alpha - 1) * (1 - t)**(alpha - 1))
+    return za
+
+
+def pde_fn(prediction, sample):
+    T = 1
+    mu = 1
+    dx = jacobian(prediction, sample, j=0)
+    dt = jacobian(prediction, sample, j=1)
+    ddx = jacobian(dx, sample, j = 0)
+    ddt = jacobian(dt, sample, j = 1)
+    return ddt - (T/mu)*ddx - f(sample)
+
+
+def ic_fn_vel(prediction, sample):
+    dt = jacobian(prediction, sample, j=1)
+    ics = torch.zeros_like(dt)
+    return dt, ics
+
+
+
+batchsize = 10000
+learning_rate = 1e-3
+
+print("Building Domain Dataset")
+domainDataset = DomainDataset([0.0]*num_inputs,[1.0]*num_inputs, 100000, period = 3)
+print("Building IC Dataset")
+icDataset = ICDataset([0.0]*(num_inputs-1),[1.0]*(num_inputs-1), 100000, period = 3)
+print("Building Validation Dataset")
+validationDataset = ValidationDataset([0.0]*num_inputs,[1.0]*num_inputs, batchsize)
+print("Building Validation IC Dataset")
+validationicDataset = ValidationICDataset([0.0]*(num_inputs-1),[1.0]*(num_inputs-1), batchsize)
+
+model = PINN([num_inputs] + [100]*3 + [1], nn.Tanh, hard_constraint).to(torch.device('cuda:0'))
+
+def init_normal(m):
+    if type(m) == torch.nn.Linear:
+        torch.nn.init.xavier_uniform_(m.weight)
+
+model.apply(init_normal)
+# optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas = (0.9,0.99),eps = 10**-15)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2)
+#scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+# optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+data = {
+    "name": "string_2inputs_nostiffness_ic0hard_icv0",
+    "model": model,
+    "epochs": epochs,
+    "batchsize": batchsize,
+    "optimizer": optimizer,
+    "scheduler": scheduler,
+    "pde_fn": pde_fn,
+    "ic_fns": [ic_fn_vel],
+    "domain_dataset": domainDataset,
+    "ic_dataset": icDataset,
+    "validation_domain_dataset": validationDataset,
+    "validation_ic_dataset": validationicDataset
+}
+
+train(data)
