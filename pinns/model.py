43ac5d8ddce0091cfc42c98f578f1d2a90e5d74f
Emanuele De Santis <trunk96@gmail.com>
2024-05-03 09:41:27 +0200

    added Random Fourier Features embedding

--- pinns/model.py
+++ pinns/model.py
@@ -1,21 +1,28 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from pinns.rff import GaussianEncoding
 from collections import OrderedDict
 
 class PINN(nn.Module):
 
-    def __init__(self, layers, activation_function, hard_constraint_fn = None):
+    def __init__(self, layers, activation_function, hard_constraint_fn = None, rff=False, sigma = None):
         super(PINN, self).__init__()
         
         # parameters
         self.depth = len(layers) - 1
         
         # set up layer order dict
-        self.activation = activation_function
-        
+        self.activation = activation_function       
         layer_list = list()
-        for i in range(self.depth - 1): 
+        self.rff = rff
+        if self.rff:
+            if sigma == None:
+                return ValueError("If Random Fourier Features embedding is on, then a sigma must be specified")
+            self.encoding = GaussianEncoding(sigma=sigma, input_size=layers[0], encoded_size=layers[0])
+            layers[0] *= 2
+
+        for i in range(self.depth - 1):
             layer_list.append(
                 ('layer_%d' % i, torch.nn.Linear(layers[i], layers[i+1]))
             )
@@ -29,8 +36,12 @@ class PINN(nn.Module):
         # deploy layers
         self.layers = torch.nn.Sequential(layerDict)
         self.hard_constraint_fn = hard_constraint_fn
+        
+        
 		
     def forward(self, x):
+        if self.rff:
+            x = self.encoding(x)
         output = self.layers(x)
         if self.hard_constraint_fn != None:
             output = self.hard_constraint_fn(x, output)
