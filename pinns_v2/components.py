5e29ebad72f11329b64fef8a85b532dbf43d7c0c
Emanuele De Santis <trunk96@gmail.com>
2024-10-22 09:40:23 +0200

    fixed missing name for ComponentManager

--- pinns_v2/components.py
+++ pinns_v2/components.py
@@ -5,6 +5,7 @@ import torch
 
 class ComponentManager(Component):
     def __init__(self) -> None:
+        super().__init__("ComponentManager")
         self._component_list_train = []
         self._component_list_valid = []
 
